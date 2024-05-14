ab1e652c02b00bd0557b2336c557f31d8c937271
cuervo-blanco <jaios.228@gmail.com>
2024-05-14 15:31:21 -0400

    Character creation: captain

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,54 @@
 // we must also perform search of that array to find a particular name
 
 use std::io;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 #[allow(dead_code)]
-struct Astronaut<String> {
+struct Astronaut {
     name: String,
-    prev: *const Astronaut<String>,
-    next: *const Astronaut<String>,
+    captain: bool,
+    prev: Option<Rc<RefCell<Astronaut>>>,
+    next: Option<Rc<RefCell<Astronaut>>>,
 }
 
-//impl Astronaut<String>{
-//    fn new(name: String) ->
-//}
+impl Astronaut{
+    fn new( name: String,
+            captain: bool,
+            _prev: Option<Rc<RefCell<Astronaut>>>,
+            _next: Option<Rc<RefCell<Astronaut>>>) -> Rc<RefCell<Self>> {
+                Rc::new(RefCell::new(Astronaut {
+                                                name,
+                                                captain,
+                                                prev: None,
+                                                next: None
+                                                }
+                                    )
+                        )
+            }
+}
+
+#[allow(dead_code)]
+fn add() -> Rc<RefCell<Astronaut>>{
+    // Ask more information about what to add and then kick
+    // other functions based on that.
+    println!("Starting creation process...");
+    println!("Enter Austronaut name; try to be nice to him: ");
+    let mut name: String = String::new();
+    let _ = io::stdin().read_line(&mut name);
+    let captain_hat: Option<Rc<RefCell<Astronaut>>> = None;
+    let captain = Astronaut::new(
+        name.trim().to_string(),
+        true,
+        captain_hat,
+        None::<Rc<RefCell<Astronaut>>>
+    );
+    {
+        let captain_ref = captain.borrow();
+        println!("Succesfully created Captain {}!", captain_ref.name);
+    }
+    captain
+}
 
 #[allow(dead_code)]
 enum OptionSelector {
@@ -50,16 +87,6 @@ impl TerminalOption{
     }
 }
 
-#[allow(dead_code)]
-fn add() {
-    // Ask more information about what to add and then kick
-    // other functions based on that.
-    println!("Adding a astronaut");
-
-}
-
-
-
 fn main() {
     // Options
    let option1 = TerminalOption {
@@ -80,7 +107,6 @@ fn main() {
     let options: Vec<TerminalOption> = vec![option1, option2, option3];
     // Astronaut linked-list
 
-    let _captain_hat: Option<Astronaut<String>> = None;
 
     // Selection algorithm
     let mut selection = String::new();
